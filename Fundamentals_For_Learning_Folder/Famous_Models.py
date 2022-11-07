f63062c060403cb5f7e490d729ffdb5a36e02f30
valeman100 <val.mannucci@gmail.com>
2022-11-07 22:06:06 +0100

    AnyNet

--- Fundamentals_For_Learning_Folder/Famous_Models.py
+++ Fundamentals_For_Learning_Folder/Famous_Models.py
@@ -317,3 +317,36 @@ class DenseNet(Classifier):
                                                   nn.AdaptiveAvgPool2d((1, 1)), nn.Flatten(),
                                                   nn.LazyLinear(num_classes)))
 
+
+class AnyNet(Classifier):
+
+    def stem(self, num_channels):
+        return nn.Sequential(nn.LazyConv2d(num_channels, kernel_size=3, stride=2, padding=1),
+                             nn.LazyBatchNorm2d(), nn.ReLU())
+
+    def stage(self, depth, num_channels, groups, bot_mul):
+        blk = []
+        for i in range(depth):
+            if i == 0:
+                blk.append(ResNeXtBlock(num_channels, bot_mul, groups, use_1x1conv=True, strides=2))
+            else:
+                blk.append(ResNeXtBlock(num_channels, bot_mul, groups))
+        return nn.Sequential(*blk)
+
+    def __init__(self, arch, stem_channels, num_classes=10):
+        super().__init__()
+        self.net = nn.Sequential(self.stem(stem_channels))
+        for i, s in enumerate(arch):
+            self.net.add_module(f'stage{i + 1}', self.stage(*s))
+        self.net.add_module('head', nn.Sequential(nn.AdaptiveAvgPool2d((1, 1)),
+                                                  nn.Flatten(), nn.LazyLinear(num_classes)))
+
+
+class RegNet32(AnyNet):
+    def __init__(self, num_classes=10):
+        stem_channels, groups, bot_mul = 32, 16, 1
+        depths, channels = (4, 6), (32, 80)
+        super().__init__(((depths[0], channels[0], groups, bot_mul),
+                          (depths[1], channels[1], groups, bot_mul)),
+                         stem_channels, num_classes)
+
