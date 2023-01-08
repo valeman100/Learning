38ed50db7611b5a55f80bc40f63d9863392eab3b
valeman100 <val.mannucci@gmail.com>
2023-01-08 15:59:41 +0100

    transformers full implementation and tested simple run

---
+++ Attention and Transformers/model.py
@@ -0,0 +1,48 @@
+import copy
+from transformer_blocks import *
+
+
+def make_model(src_vocab, tgt_vocab, N=6, d_model=512, d_ff=2048, h=8, dropout=0.1):
+    c = copy.deepcopy
+    attn = MultiHeadedAttention(h, d_model)
+    ff = PositionwiseFeedForward(d_model, d_ff, dropout)
+    position = PositionalEncoding(d_model, dropout)
+    model = EncoderDecoder(
+        Encoder(EncoderLayer(d_model, c(attn), c(ff), dropout), N),
+        Decoder(DecoderLayer(d_model, c(attn), c(attn), c(ff), dropout), N),
+        nn.Sequential(Embeddings(d_model, src_vocab), c(position)),
+        nn.Sequential(Embeddings(d_model, tgt_vocab), c(position)),
+        Generator(d_model, tgt_vocab),
+    )
+
+    for p in model.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform_(p)
+
+    return model
+
+
+def inference_test():
+    test_model = make_model(11, 11, 2).eval()
+    src = torch.LongTensor([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]])
+    src_mask = torch.ones(1, 1, 10)
+
+    memory = test_model.encode(src, src_mask)
+    ys = torch.zeros(1, 1).type_as(src)
+
+    for i in range(9):
+        out = test_model.decode(memory, src_mask, ys, subsequent_mask(ys.size(1)).type_as(src.data))
+        prob = test_model.generator(out[:, -1])
+        _, next_word = torch.max(prob, dim=1)
+        next_word = next_word.data[0]
+        ys = torch.cat([ys, torch.empty(1, 1).type_as(src.data).fill_(next_word)], dim=1)
+
+        print('Untrained model predictions:', ys)
+
+
+def run_tests(N=10):
+    for _ in range(N):
+        inference_test()
+
+
+run_tests()
--- Attention and Transformers/transformer_blocks.py
+++ Attention and Transformers/transformer_blocks.py
@@ -1,28 +1,30 @@
-import os
-from os.path import exists
 import torch
 import torch.nn as nn
-from torch.nn.functional import log_softmax, pad
+from torch.nn.functional import log_softmax
 import math
 import copy
-import time
-from torch.optim.lr_scheduler import LambdaLR
-import pandas as pd
-import altair as alt
-from torchtext.data.functional import to_map_style_dataset
-from torch.utils.data import DataLoader
-from torchtext.vocab import build_vocab_from_iterator
-import torchtext.datasets as datasets
-import spacy
-import GPUtil
-import warnings
-from torch.utils.data.distributed import DistributedSampler
-import torch.distributed as dist
-import torch.multiprocessing as mp
-from torch.nn.parallel import DistributedDataParallel as DDP
 
-# Set to False to skip notebook execution (e.g. for debugging)
-warnings.filterwarnings("ignore")
+
+# import time
+# from torch.optim.lr_scheduler import LambdaLR
+# import pandas as pd
+# import altair as alt
+# from torchtext.data.functional import to_map_style_dataset
+# from torch.utils.data import DataLoader
+# from torchtext.vocab import build_vocab_from_iterator
+# import torchtext.datasets as datasets
+# import spacy
+# import GPUtil
+# import warnings
+# from torch.utils.data.distributed import DistributedSampler
+# import torch.distributed as dist
+# import torch.multiprocessing as mp
+# from torch.nn.parallel import DistributedDataParallel as DDP
+
+# warnings.filterwarnings("ignore")
+
+
+# reference: http://nlp.seas.harvard.edu/annotated-transformer/
 
 
 class EncoderDecoder(nn.Module):
@@ -32,7 +34,7 @@ class EncoderDecoder(nn.Module):
         self.decoder = decoder
         self.src_embed = src_embed
         self.tgt_embed = tgt_embed
-        self.genertor = generator
+        self.generator = generator
 
     def forward(self, src, tgt, src_mask, tgt_mask):
         return self.decode(self.encode(src, src_mask), src_mask, tgt, tgt_mask)
@@ -112,7 +114,7 @@ class EncoderLayer(nn.Module):
 class Decoder(nn.Module):
     def __init__(self, layer, N):
         super().__init__()
-        self.layer = clones(layer, N)
+        self.layers = clones(layer, N)
         self.norm = LayerNorm(layer.size)
 
     def forward(self, x, memory, src_mask, tgt_mask):
@@ -144,4 +146,81 @@ def subsequent_mask(size):
     return subsequent_mask == 0
 
 
+def attention(query, key, value, mask=None, dropout=None):
+    d_k = query.size(-1)
+    scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
+    if mask is not None:
+        # where you find 0 put -1e9
+        scores = scores.masked_fill(mask == 0, -1e9)
+    p_attn = scores.softmax(dim=-1)
+    if dropout is not None:
+        p_attn = dropout(p_attn)
+    return torch.matmul(p_attn, value), p_attn
 
+
+class MultiHeadedAttention(nn.Module):
+    def __init__(self, h, d_model, dropout=0.1):
+        super().__init__()
+        assert d_model % h == 0  # we assume d_v always equal to d_k
+        self.d_k = d_model // h
+        self.h = h  # number of heads
+        self.linears = clones(nn.Linear(d_model, d_model), 4)
+        self.attn = None
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, query, key, value, mask=None):
+        if mask is not None:
+            # apply mask to all heads
+            mask = mask.unsqueeze(1)
+        nbatches = query.size(0)
+
+        query, key, value = [lin(x).view(nbatches, -1, self.h, self.d_k).transpose(1, 2)
+                             for lin, x in zip(self.linears, (query, key, value))]
+
+        x, self.attn = attention(query, key, value, mask=mask, dropout=self.dropout)
+
+        x = (x.transpose(1, 2).contiguous().view(nbatches, -1, self.h * self.d_k))
+
+        del key
+        del query
+        del value
+        return self.linears[-1](x)
+
+
+class PositionwiseFeedForward(nn.Module):
+    def __init__(self, d_model, d_ff, dropout=0.1):
+        super().__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, x):
+        return self.w_2(self.dropout(self.w_1(x).relu()))
+
+
+class Embeddings(nn.Module):
+    def __init__(self, d_model, vocab):
+        super().__init__()
+        self.lut = nn.Embedding(vocab, d_model)
+        self.d_model = d_model
+
+    def forward(self, x):
+        return self.lut(x) * math.sqrt(self.d_model)
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout, max_len=5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * -(math.log(10000) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        # non lernable but registered in the model state dict
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:, : x.size(1)].requires_grad_(False)
+        return self.dropout(x)
